76f15654a1754f2ea38e4b44001601d90c232196
AxelBone <axel.bonesteve@gmail.com>
2025-09-25 16:46:21 +0200

    Update dataset_origin, genereviews extraction

---
+++ data/public_db/dataset_origin.sh
@@ -0,0 +1,16 @@
+################################
+
+# DATASETS ORIGIN
+
+###############################
+
+## Clinvar
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20250923.vcf.gz
+
+## Mitocarta
+# https://personal.broadinstitute.org/scalvo/MitoCarta3.0/human.mitocarta3.0.html
+
+## GeneReviews
+wget https://ftp.ncbi.nlm.nih.gov/pub/litarch/ca/84/gene_NBK1116.tar.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/GeneReviews/NBKid_shortname_genesymbol.txt
+
